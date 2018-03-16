82c067df7fafc685f63f852d2bbb5373ca6b85be
ElizavetaKovalchuk <elizkovalchuk13@gmail.com>
2018-03-16 16:54:30 +0300

    DEV-2_tests

---
+++ DEV_2_Tests/DEV_2.Tests/CreatorTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xunit;
+using DEV_2;
+
+namespace DEV_2.Tests
+{
+    public class CreatorTest
+    {
+        [Fact]
+        public void Output_EmptyString_Null()
+        {
+            string enteredString = string.Empty;
+            Creator c = new Creator(enteredString);
+            Assert.Null(c.CreateSequenceOfOddSymbol(enteredString));
+        }
+
+        [Fact]
+        public void OutputElements_enteredString_OddElements()
+        {
+            string enteredString = "Elizaveta";
+            string outputString = "Eiaea";
+            Creator c = new Creator(enteredString);
+            Assert.Equal(c.CreateSequenceOfOddSymbol(enteredString), outputString);
+        }
+
+        [Fact]
+        public void OutputElements_oneElement_oneElement()
+        {
+            string enteredString = "E";
+            string outputString = "E";
+            Creator c = new Creator(enteredString);
+            Assert.Equal(c.CreateSequenceOfOddSymbol(enteredString), outputString);
+        }
+
+        public void OutputSttring_typeString_typeString()
+        {
+            string enteredString = "Elizaveta";
+            Creator c = new Creator(enteredString);
+            Assert.IsType(typeof(String), c.CreateSequenceOfOddSymbol(enteredString));
+        }
+    }
+}
+
