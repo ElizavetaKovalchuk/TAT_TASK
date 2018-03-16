7c31c64a52ac153eb2fde77902e6d21e352e7d4e
ElizavetaKovalchuk <elizkovalchuk13@gmail.com>
2018-03-16 16:32:07 +0300

    DEV-2

---
+++ DEV-2/DEV-2/Creator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace DEV_2
+{
+    /// <summary>
+    /// Serch elements with odd indeks in a string and create string
+    /// </summary>
+    public class Creator
+    {
+        public string sequnceOfSymbol;
+        public Creator(string enteredSequence)
+        {
+            sequnceOfSymbol = enteredSequence;
+        }
+        /// <summary>
+        /// Serch elements with odd indeks in a string and create string
+        /// </summary>
+        /// <returns> string of elements with odd indeks</returns>
+        public string CreateSequenceOfOddSymbol()
+        {
+            StringBuilder oddElements = new StringBuilder();
+            for (int i = 0; i < sequnceOfSymbol.Length; i += 2)
+            {
+                oddElements.Append(sequnceOfSymbol[i]);
+            }
+            return oddElements.ToString();
+        }
+    }
+}
---
+++ DEV-2/DEV-2/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace DEV_2
+{
+    class EntryPoint
+    {
+        /// <summary>
+        /// It is an entry point of the program and it is outputs elements with odd indeks
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string line = Console.ReadLine();
+            Creator enteredSequence = new Creator(line);
+            Console.WriteLine(enteredSequence.CreateSequenceOfOddSymbol());
+        }
+    }
+}
