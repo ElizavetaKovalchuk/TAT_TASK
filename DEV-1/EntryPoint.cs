0da5677beafe80a27776f43af0a7183fdc51b4b3
ElizavetaKovalchuk <elizkovalchuk13@gmail.com>
2018-03-16 16:23:58 +0300

    DEV-1

---
+++ DEV-1/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_1
+{
+    class Program
+    {
+        /// <summary>
+        /// Prints to the console the maximum number of identical consecutive symbols in the string
+        /// </summary>
+        /// <param name="args">Сommand-line arguments</param>
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter a numeric argument.");
+            }
+            else
+            {
+                SequenceOfSymbol acceptedSequence = new SequenceOfSymbol(args[0]);
+                Console.WriteLine(acceptedSequence.SerchMaxNumberOfSymbol());
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
